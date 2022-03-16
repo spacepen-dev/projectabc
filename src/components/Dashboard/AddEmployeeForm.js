e61e43007caefee69cac456dd319620ff878a7e6
Thomas <ejembithomas61@gmail.com>
2022-03-16 16:21:57 +0100

    add employee progreess bar functionality added and bug resolved

--- src/components/Dashboard/AddEmployeeForm.js
+++ src/components/Dashboard/AddEmployeeForm.js
@@ -1,20 +1,29 @@
 import React, { useEffect, useState } from "react";
-import { Form } from "react-bootstrap";
-import { ProgressBar } from "react-bootstrap";
+import { Form, ProgressBar } from "react-bootstrap";
 
 const AddEmployeeForm = ({ currentForm }) => {
   const [active, setActive] = useState("active");
+  const [progress, setProgress] = useState("33%");
 
   useEffect(() => {
     setActive("active");
-  }, []);
+    if (currentForm === 2) {
+      setProgress("67%");
+    } else if (currentForm === 3) {
+      setProgress("100%");
+    } else if (currentForm === 1) {
+      setProgress("33%");
+    }
+  }, [currentForm]);
 
   return (
     <div>
-      <div className="d-flex flex-column align-items-center">
-        <div className="range-container">
-          <div className="d-flex justify-content-evenly align-items-center w-100">
-            <div className="p-3 d-flex flex-column justify-content-center align-items-center">
+      {console.log(currentForm)}
+      {console.log(progress)}
+      <div className='d-flex flex-column align-items-center'>
+        <div className='range-container'>
+          <div className='d-flex justify-content-evenly align-items-center w-100'>
+            <div className='p-3 d-flex flex-column justify-content-center align-items-center'>
               <Form.Label>Profile</Form.Label>
               {/* <span
                 className={`d-inline-block rounded-circle mt-2 ${
@@ -23,8 +32,8 @@ const AddEmployeeForm = ({ currentForm }) => {
                 id="profile"
               ></span> */}
             </div>
-            <small className="d-inline-block"></small>
-            <div className="p-3 d-flex flex-column justify-content-center align-items-center">
+            <small className='d-inline-block'></small>
+            <div className='p-3 d-flex flex-column justify-content-center align-items-center'>
               <Form.Label>Salary</Form.Label>
               {/* <span
                 className={`d-inline-block rounded-circle mt-2  ${
@@ -33,8 +42,8 @@ const AddEmployeeForm = ({ currentForm }) => {
                 id="contact"
               ></span> */}
             </div>
-            <small className="d-inline-block"></small>
-            <div className="p-3 d-flex flex-column justify-content-center align-items-center">
+            <small className='d-inline-block'></small>
+            <div className='p-3 d-flex flex-column justify-content-center align-items-center'>
               <Form.Label>Account</Form.Label>
               {/* <span
                 className={`d-inline-block rounded-circle mt-2  ${
@@ -44,8 +53,8 @@ const AddEmployeeForm = ({ currentForm }) => {
               ></span> */}
             </div>
           </div>
-          <div className="progress">
-            <ProgressBar animated now={67} style={{ width: "40%" }} />
+          <div className='progress'>
+            <ProgressBar animated now={100} style={{ width: `${progress}` }} />
           </div>
         </div>
       </div>
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -42,39 +42,38 @@ const Dashboard = ({ FetchDepartment }) => {
 
   return (
     <Container fluid>
-      <Row className="row">
-        <Navbar bg="light" className="nav-bar" expand={false}>
+      <Row className='row'>
+        <Navbar bg='light' className='nav-bar' expand={false}>
           <Container fluid>
-            <Navbar.Toggle aria-controls="offcanvasNavbar" />
+            <Navbar.Toggle aria-controls='offcanvasNavbar' />
             <Navbar.Offcanvas
-              id="offcanvasNavbar"
-              className="sidebar-menu"
-              aria-labelledby="offcanvasNavbarLabel"
-              placement="start"
-            >
+              id='offcanvasNavbar'
+              className='sidebar-menu'
+              aria-labelledby='offcanvasNavbarLabel'
+              placement='start'>
               <Offcanvas.Header closeButton>
-                <Offcanvas.Title id="offcanvasNavbarLabel"></Offcanvas.Title>
+                <Offcanvas.Title id='offcanvasNavbarLabel'></Offcanvas.Title>
               </Offcanvas.Header>
               <SideBar pageId={(id) => setPage(id)} page={page} />
             </Navbar.Offcanvas>
           </Container>
         </Navbar>
-        <Col className="d-none d-lg-block col-1">
+        <Col className='d-none d-lg-block col-1'>
           <SideBar pageId={(id) => setPage(id)} page={page} />
         </Col>
-        <Col className="col-2">
+        <Col className='col-2'>
           <Routes>
             <Route
               index
               element={<Overview getPageId={(id) => setPage(id)} />}
             />
             <Route
-              path="/overview"
+              path='/overview'
               element={<Overview getPageId={(id) => setPage(id)} />}
             />
-//             <Route path="/add/employee" element={<AddEmployee />} />
-//             <Route path="/view/employees" element={<ViewEmployee />} />
-//             <Route path="/view/salary/history" element={<ViewSalaryHisory />} />
+            {/* //             <Route path="/add/employee" element={<AddEmployee />} /> */}
+            {/* //             <Route path="/view/employees" element={<ViewEmployee />} /> */}
+            {/* //             <Route path="/view/salary/history" element={<ViewSalaryHisory />} /> */}
             <Route path='/add/employee' element={<AddEmployee />} />
             <Route path='/view/employees' element={<ViewEmployee />} />
             <Route
@@ -88,13 +87,12 @@ const Dashboard = ({ FetchDepartment }) => {
             <Route path='/view/tax/history' element={<ViewTaxHistory />} />
 
             <Route
-              path="/pay/employee/salaries"
+              path='/pay/employee/salaries'
               element={<EmployeeSalariesPage />}
             />
 
-            <Route path="/company/profile" element={<CompanyProfile />} />
+            <Route path='/company/profile' element={<CompanyProfile />} />
             <Route path='/top/up' element={<CompanyWalletPage />} />
-
           </Routes>
         </Col>
       </Row>
