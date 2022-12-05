696373f36149b012965da14fbf72bd8f5f8ee9a5
apple <apple@apples-MacBook-Pro.local>
2022-12-05 05:47:10 -0800

    changes

--- src/components/Dashboard/company-profile/CompanyProfile.js
+++ src/components/Dashboard/company-profile/CompanyProfile.js
@@ -170,146 +170,142 @@ class CompanyProfile extends React.Component {
     };
 
     return (
-      <div>
-        <div>
-          <h4 className='entire-page-headers'>COMPANY DETAILS </h4>
-        </div>
-        <div className='profile-info'>
-          <div className='company-logo-div'>
-            <div id='displayCompanyLogo'>
-              <img
-                // src={EditableLogo}
-                alt='company logo'
-                width={100}
-                height={100}
-                className='company-logo'
-              />
-            </div>
-            <div className='logo-svg-div'>
-              <label for='pen-img-label'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='16'
-                  height='16'
-                  fill='#23549e'
-                  class='bi bi-pencil'
-                  viewBox='0 0 16 16'
-                  id='image-edit'>
-                  <path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z' />
-                </svg>
-              </label>
-            </div>
-          </div>
-          <input
-            type='file'
-            accept='image/*'
-            id='pen-img-label'
-            style={{ display: "none" }}
-            // onChange={() => GetImage()}
-          />
-          {/* {console.log(this.props.)} */}
-          <h5>{this.state.companyData.CompanyName}</h5>
-          <div>
-            <div class='profile-texts'>
-              {this.state.companyData.emailAddress}
-            </div>
-            <div class='profile-texts'>
-              company Size: {this.state.companyData.companySize}
-            </div>
-            <div class='profile-texts'>{this.state.companyData.website}</div>
-            <div class='profile-texts'>
-              employees: {this.state.companyData.totalEmployees}{" "}
-            </div>
-          </div>
-          <p class='freewill-address'>{this.state.companyData.address}</p>
-        </div>
-        {/* <p class='freewill-address'></p> */}
+			<div>
+				<div>
+					<h4 className="entire-page-headers">COMPANY DETAILS </h4>
+				</div>
+				<div className="profile-info">
+					<div className="company-logo-div">
+						<div id="displayCompanyLogo">
+							<img
+								// src={EditableLogo}
+								alt="company logo"
+								width={100}
+								height={100}
+								className="company-logo"
+							/>
+						</div>
+						<div className="logo-svg-div">
+							<label for="pen-img-label">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="16"
+									height="16"
+									fill="#23549e"
+									class="bi bi-pencil"
+									viewBox="0 0 16 16"
+									id="image-edit">
+									<path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
+								</svg>
+							</label>
+						</div>
+					</div>
+					<input
+						type="file"
+						accept="image/*"
+						id="pen-img-label"
+						style={{ display: "none" }}
+						// onChange={() => GetImage()}
+					/>
+					{/* {console.log(this.props.)} */}
+					<h5>{this.state.companyData.CompanyName}</h5>
+					<div>
+						<div class="profile-texts">
+							{this.state.companyData.emailAddress}
+						</div>
+						<div class="profile-texts">
+							company Size: {this.state.companyData.companySize}
+						</div>
+						<div class="profile-texts">{this.state.companyData.website}</div>
+						<div class="profile-texts">
+							employees: {this.state.companyData.totalEmployees}{" "}
+						</div>
+					</div>
+					<p class="freewill-address">{this.state.companyData.address}</p>
+				</div>
+				{/* <p class='freewill-address'></p> */}
 
-        <div>
-          <div className='profile-about-div'>
-            <h6 class='profile-subHeads'>ABOUT</h6>
-            <div className='about-svg-div'>
-              <svg
-                // onClick={() => ProfileEdit()}
-                onClick={() => this.setState({ page: this.state.page + 1 })}
-                xmlns='http://www.w3.org/2000/svg'
-                width='16'
-                height='16'
-                fill='#23549e'
-                class='bi bi-pencil'
-                viewBox='0 0 16 16'
-                id='profile-edit'>
-                <path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z' />
-              </svg>
-            </div>
-          </div>
+				<div>
+					<div className="profile-about-div">
+						<h6 class="profile-subHeads">ABOUT</h6>
+						<div className="about-svg-div">
+							<svg
+								// onClick={() => ProfileEdit()}
+								onClick={() => this.setState({ page: this.state.page + 1 })}
+								xmlns="http://www.w3.org/2000/svg"
+								width="16"
+								height="16"
+								fill="#23549e"
+								class="bi bi-pencil"
+								viewBox="0 0 16 16"
+								id="profile-edit">
+								<path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
+							</svg>
+						</div>
+					</div>
 
-          <p class='freewill-about company-about'>
-            {this.state.companyData.about}
-          </p>
-        </div>
+					<p class="freewill-about company-about">
+						{this.state.companyData.about}
+					</p>
+				</div>
 
-        <div class='container-flex'>
-          <div class='column left'>
-            <div className='profile-about-div'>
-              <h6 class='profile-subHeads'>DEPARTMENTS</h6>
-              <div className='about-svg-div'>
-                <svg
-                  // onClick={() => ProfileEdit()}
-                  onClick={() => this.setState({ page: this.state.page + 2 })}
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='16'
-                  height='16'
-                  fill='#23549e'
-                  class='bi bi-pencil'
-                  viewBox='0 0 16 16'
-                  id='profile-edit'>
-                  <path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z' />
-                </svg>
-              </div>
-            </div>
-            <div>{this.displayDepartment()}</div>
-          </div>
+				<div class="container-flex">
+					<div class="column left">
+						<div className="profile-about-div">
+							<h6 class="profile-subHeads">DEPARTMENTS</h6>
+							<div className="about-svg-div">
+								<svg
+									// onClick={() => ProfileEdit()}
+									onClick={() => this.setState({ page: this.state.page + 2 })}
+									xmlns="http://www.w3.org/2000/svg"
+									width="16"
+									height="16"
+									fill="#23549e"
+									class="bi bi-pencil"
+									viewBox="0 0 16 16"
+									id="profile-edit">
+									<path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
+								</svg>
+							</div>
+						</div>
+						<div>{this.displayDepartment()}</div>
+					</div>
 
-          <div class='column right'>
-            <h6 class='profile-subHeads'>ROLES</h6>
+					<div class="column right">
+						<h6 class="profile-subHeads">ROLES</h6>
 
-            <div>
-              <div>{this.displayRoles()}</div>
-            </div>
-          </div>
-        </div>
+						<div>
+							<div>{this.displayRoles()}</div>
+						</div>
+					</div>
+				</div>
 
-        <h6 class='profile-subHeads'>ACCOUNT INFORMATION</h6>
-        <div class='container-flex'>
-          <div class='freewill-acct'>
-            <p class='freewill-acct-info fw-bolder'>Account Number</p>
-            <p class='freewill-acct-details'>
-              {this.state.companyData.accountNumber}
-            </p>
-          </div>
-
-          <div class='freewill-acct'>
-            <p class='freewill-acct-info fw-bolder'>Account Name</p>
-            <p class='freewill-acct-details details-last'>
-              {this.state.companyData.CompanyName}
-            </p>
-          </div>
-
-          {/* <div class='freewill-acct'>
+				<h6 class="profile-subHeads">ACCOUNT INFORMATION</h6>
+				<div class="container-flex">
+					<div class="freewill-acct">
+						<p class="freewill-acct-info fw-bolder">Account Number</p>
+						<p class="freewill-acct-details">
+							{this.state.companyData.accountNumber}
+						</p>
+					</div>
+					<div class="freewill-acct">
+						<p class="freewill-acct-info fw-bolder">Account Name</p>
+						<p class="freewill-acct-details details-last">
+							{this.state.companyData.CompanyName}
+						</p>
+					</div>
+					{/* <div class='freewill-acct'>
             <p class='freewill-acct-info'>Bank Name</p>
             <p class='freewill-acct-details'>
               {this.state.companyData.bankName}
             </p>
           </div> */}
-
-          {this.state.errMessage && (
-            
-          )}
-        </div>
-        {this.state.request && <FullScreenLoader />}
-      </div>
-    );
+					{/* {this.state.errMessage && ( */}
+					{/* )} */}
+				</div>
+				{this.state.request && <FullScreenLoader />}
+			</div>
+		);
   }
 }
 
